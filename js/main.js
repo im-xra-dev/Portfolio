94b25321a70e67663e754c3787f38f74335f7071
Charlie <charlieferris15@gmail.com>
2022-02-25 19:00:08 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-function redir(to){
-    let url = "http://"+window.location.host+"/Portfolio"+to;
-    window.location.href = url;
+function expand(type) {
+  const mode = $(`#${type}`).children('.experience-bubble-buttons').children('a').html()
+
+  const expandedDisplay = mode === 'expand' ? 'show' : 'hide';
+  const collapsedDisplay = mode === 'collapsed' ? 'show' : 'hide';
+
+  $(`#${type}-desc-big`).removeClass(collapsedDisplay)
+  $(`#${type}-desc-big`).addClass(expandedDisplay)
+
+  $(`#${type}-desc-small`).addClass(collapsedDisplay)
+  $(`#${type}-desc-small`).removeClass(expandedDisplay)
+
+  $(`#${type}`).children('.experience-bubble-buttons').children('a.expandbtn').html(mode === 'expand' ? 'collapse' : 'expand')
 }
